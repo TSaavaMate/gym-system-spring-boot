a2cbb35008e2be58d8d2a9b1f779f8f71e757ffa
TsaavaMate <caavamate@gmail.com>
2024-03-15 21:25:29 +0400

    refactor loggerAspect

--- src/main/java/com/example/jwtdemo/aspect/LoggingAspect.java
+++ src/main/java/com/example/jwtdemo/aspect/LoggingAspect.java
@@ -10,7 +10,6 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.boot.logging.LogLevel;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
@@ -28,98 +27,83 @@ public class LoggingAspect {
     private final UserRepository userRepository;
 
     @Pointcut("@annotation(Loggable)")
-    public void executeLogging(){}
+    public void executeLogging() {}
 
     @Around(value = "executeLogging()")
     public Object logMethodCall(ProceedingJoinPoint joinPoint) throws Throwable {
-
         String transactionId = generateTransactionId();
-
-        // Log the start of the transaction
         log.info("Transaction started with ID: {}", transactionId);
 
-        var timeStamp = LocalDateTime.now();
-        var logLevel = LogLevel.INFO;
-
         Throwable exception = null;
-
         Object returnValue = null;
+
         try {
             returnValue = joinPoint.proceed();
-
             log.info("Transaction completed with ID: {}", transactionId);
-
         } catch (Throwable e) {
-            logLevel = LogLevel.ERROR;
             exception = e;
-
             log.error("Transaction failed with ID: {}", transactionId);
         }
 
-        String stackTrace = (exception == null)
-                ? null
-                : Arrays.toString(exception.getStackTrace());
+        logMethodDetails(joinPoint, returnValue, exception);
+        return returnValue;
+    }
 
-        StringBuilder message = new StringBuilder("Method: ");
-        message.append(joinPoint.getSignature().getName());
-
-        Object[] args = joinPoint.getArgs();
-
-        if (null!=args && args.length>0){
-            message.append(" args=[ | ");
-            Arrays.asList(args).forEach(arg-> message.append(arg).append(" | "));
-            message.append("]");
-        }
-
-        if(returnValue instanceof Collection){
-            message.append(", returning: ").append(((Collection<?>)returnValue).size()).append(" instance(s)");
-        }else{
-            message.append(", returning: ").append(returnValue != null ? returnValue.toString() : null);
-        }
-
-
+    private void logMethodDetails(ProceedingJoinPoint joinPoint, Object returnValue, Throwable exception) throws Throwable {
         var methodName = joinPoint.getSignature().getName();
-
-        var loggerName = logRepository.getClass().getName();
-
+        var message = buildMessage(joinPoint, returnValue);
+        var timeStamp = LocalDateTime.now();
+        var logLevel = (exception == null) ? LogLevel.INFO : LogLevel.ERROR;
+        var stackTrace = (exception == null) ? null : Arrays.toString(exception.getStackTrace());
         var threadName = Thread.currentThread().getName();
-
         var className = joinPoint.getClass().getName();
-
-
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
 
         var user = userRepository.findByEmail(authentication.getName())
                 .orElseThrow(ResourceNotFoundException::new);
 
         var userId = user.getId();
 
-
-
         var entry = LogEntry.builder()
                 .timestamp(timeStamp)
-                .loggerName(loggerName)
+                .loggerName(logRepository.getClass().getName())
                 .logLevel(logLevel.toString())
                 .threadName(threadName)
                 .className(className)
                 .methodName(methodName)
-                .message(message.toString())
+                .message(message)
                 .exceptionStackTrace(stackTrace)
                 .userId(userId)
                 .build();
 
-
         logRepository.save(entry);
 
-        if (exception != null){
-           throw exception;
+        if (exception != null) {
+            throw exception;
+        }
+    }
+
+    private String buildMessage(ProceedingJoinPoint joinPoint, Object returnValue) {
+        StringBuilder message = new StringBuilder("Method: ");
+        message.append(joinPoint.getSignature().getName());
+        Object[] args = joinPoint.getArgs();
+
+        if (null != args && args.length > 0) {
+            message.append(" args=[ | ");
+            Arrays.asList(args).forEach(arg -> message.append(arg).append(" | "));
+            message.append("]");
         }
 
-
-        return returnValue;
+        if (returnValue instanceof Collection) {
+            message.append(", returning: ").append(((Collection<?>) returnValue).size()).append(" instance(s)");
+        } else {
+            message.append(", returning: ").append(returnValue != null ? returnValue.toString() : null);
+        }
+        return message.toString();
     }
 
     private String generateTransactionId() {
         return UUID.randomUUID().toString();
     }
 }
+
