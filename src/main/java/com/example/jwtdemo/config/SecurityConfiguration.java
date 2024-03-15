1e72c05e3d5031b749d30a699699e20f6c7771b2
TsaavaMate <caavamate@gmail.com>
2024-03-15 21:27:34 +0400

    extract duplicate code from SecurityConfiguration

--- src/main/java/com/example/jwtdemo/config/SecurityConfiguration.java
+++ src/main/java/com/example/jwtdemo/config/SecurityConfiguration.java
@@ -11,50 +11,45 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
-
     private final AuthenticationProvider authenticationProvider;
 
     @Bean
     @Profile("dev")
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->
-                    auth.requestMatchers("api/v1/auth/**")
-                            .permitAll()
-                            .anyRequest()
-                            .permitAll()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
-                .authenticationProvider(authenticationProvider)
-                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
-    }
-    @Bean
-    @Profile("!dev")
-    public SecurityFilterChain securityFilterChainProd(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth->
+    public SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
+        return configureSecurityFilterChain(http)
+                .authorizeHttpRequests(auth ->
                         auth.requestMatchers("api/v1/auth/**")
                                 .permitAll()
                                 .anyRequest()
-                                .authenticated()
-                )
+                                .permitAll())
+                .build();
+    }
+
+    @Bean
+    @Profile("!dev")
+    public SecurityFilterChain securityFilterChainProd(HttpSecurity http) throws Exception {
+        return configureSecurityFilterChain(http)
+                .authorizeHttpRequests(auth ->
+                        auth.requestMatchers("api/v1/auth/**")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated())
+                .build();
+    }
+
+    private HttpSecurity configureSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                )
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
-
-        return http.build();
     }
 }
+
